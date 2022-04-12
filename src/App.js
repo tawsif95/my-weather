cc1af9045596cb428f7a63db240ae0dcbede23af
tawsif95 <tawsifrafid765@gmail.com>
2022-04-12 17:23:31 -0500

    Current weather - requires locations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import './components/weather'
+import WeatherCard from './components/weather';
+import { CircularProgress } from '@mui/material';
 
-function App() {
+const App = () => {
+
+  const[lat, setLat] = useState([]);
+  const[long, setLong] = useState([]);
+  const[data, setData] = useState([]);
+  
+  useEffect(() => {
+
+    const fetchData = async () => {
+
+      navigator.geolocation.getCurrentPosition( function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+      
+      await fetch(`${process.env.REACT_APP_API_URL}/weather?lat=${lat}&lon=${long}&appid=${process.env.REACT_APP_API_KEY}`)
+        .then(res => res.json())
+        .then(res => {
+          console.log(res);
+          setData(res);
+        })
+    };
+
+    fetchData();
+  },[lat, long]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        (typeof data.main != 'undefined') ? (
+          <WeatherCard weatherData={data}/>
+        ) : (
+          <div>
+            <CircularProgress color="secondary" />
+          </div>
+        )
+      }
     </div>
   );
 }
